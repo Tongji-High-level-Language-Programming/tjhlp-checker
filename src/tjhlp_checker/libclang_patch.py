c21b6f67182afd854121dddcd4783af79cdb0b3d
Maoyao Ye <maoyaoye@foxmail.com>
2026-01-21 18:33:07 +0800

    feat: update to Clang 21 and improve libclang integration

--- src/tjhlp_checker/libclang_patch.py
+++ src/tjhlp_checker/libclang_patch.py
@@ -1,161 +1,38 @@
-from clang.cindex import BaseEnumeration, Cursor, c_int
-from clang.cindex import functionList, conf  # type: ignore
-
-functionList.append(("clang_getCursorBinaryOperatorKind", [Cursor], c_int))
-functionList.append(("clang_getUnaryOperatorKindSpelling", [Cursor], c_int))
-
-
-class BinaryOperator(BaseEnumeration):
-    """
-    Describes the BinaryOperator of a declaration
-    """
-
-    # The required BaseEnumeration declarations.
-    _kinds = []
-    _name_map = None
-
-    def __nonzero__(self):
-        """Allows checks of the kind ```if cursor.binary_operator:```"""
-        return self.value != 0
-
-    @property
-    def is_assignment(self):
-        return BinaryOperator.Assign.value <= self.value < BinaryOperator.Comma.value
-
-    Invalid: "BinaryOperator"
-    PtrMemD: "BinaryOperator"
-    PtrMemI: "BinaryOperator"
-    Mul: "BinaryOperator"
-    Div: "BinaryOperator"
-    Rem: "BinaryOperator"
-    Add: "BinaryOperator"
-    Sub: "BinaryOperator"
-    Shl: "BinaryOperator"
-    Shr: "BinaryOperator"
-    Cmp: "BinaryOperator"
-    LT: "BinaryOperator"
-    GT: "BinaryOperator"
-    LE: "BinaryOperator"
-    GE: "BinaryOperator"
-    EQ: "BinaryOperator"
-    NE: "BinaryOperator"
-    And: "BinaryOperator"
-    Xor: "BinaryOperator"
-    Or: "BinaryOperator"
-    LAnd: "BinaryOperator"
-    LOr: "BinaryOperator"
-    Assign: "BinaryOperator"
-    MulAssign: "BinaryOperator"
-    DivAssign: "BinaryOperator"
-    RemAssign: "BinaryOperator"
-    AddAssign: "BinaryOperator"
-    SubAssign: "BinaryOperator"
-    ShlAssign: "BinaryOperator"
-    ShrAssign: "BinaryOperator"
-    AndAssign: "BinaryOperator"
-    XorAssign: "BinaryOperator"
-    OrAssign: "BinaryOperator"
-    Comma: "BinaryOperator"
-
-
-BinaryOperator.Invalid = BinaryOperator(0)
-BinaryOperator.PtrMemD = BinaryOperator(1)
-BinaryOperator.PtrMemI = BinaryOperator(2)
-BinaryOperator.Mul = BinaryOperator(3)
-BinaryOperator.Div = BinaryOperator(4)
-BinaryOperator.Rem = BinaryOperator(5)
-BinaryOperator.Add = BinaryOperator(6)
-BinaryOperator.Sub = BinaryOperator(7)
-BinaryOperator.Shl = BinaryOperator(8)
-BinaryOperator.Shr = BinaryOperator(9)
-BinaryOperator.Cmp = BinaryOperator(10)
-BinaryOperator.LT = BinaryOperator(11)
-BinaryOperator.GT = BinaryOperator(12)
-BinaryOperator.LE = BinaryOperator(13)
-BinaryOperator.GE = BinaryOperator(14)
-BinaryOperator.EQ = BinaryOperator(15)
-BinaryOperator.NE = BinaryOperator(16)
-BinaryOperator.And = BinaryOperator(17)
-BinaryOperator.Xor = BinaryOperator(18)
-BinaryOperator.Or = BinaryOperator(19)
-BinaryOperator.LAnd = BinaryOperator(20)
-BinaryOperator.LOr = BinaryOperator(21)
-BinaryOperator.Assign = BinaryOperator(22)
-BinaryOperator.MulAssign = BinaryOperator(23)
-BinaryOperator.DivAssign = BinaryOperator(24)
-BinaryOperator.RemAssign = BinaryOperator(25)
-BinaryOperator.AddAssign = BinaryOperator(26)
-BinaryOperator.SubAssign = BinaryOperator(27)
-BinaryOperator.ShlAssign = BinaryOperator(28)
-BinaryOperator.ShrAssign = BinaryOperator(29)
-BinaryOperator.AndAssign = BinaryOperator(30)
-BinaryOperator.XorAssign = BinaryOperator(31)
-BinaryOperator.OrAssign = BinaryOperator(32)
-BinaryOperator.Comma = BinaryOperator(33)
-
-
-@property
-def binary_operator(self) -> BinaryOperator:
-    """
-    Retrieves the opcode if this cursor points to a binary operator
-    :return:
-    """
-    if not hasattr(self, "_binopcode"):
-        self._binopcode = conf.lib.clang_getCursorBinaryOperatorKind(self)
-    return BinaryOperator.from_id(self._binopcode)
-
-
-Cursor.binary_operator = binary_operator  # type: ignore
-
+from clang.cindex import (
+    BaseEnumeration,
+    Cursor,
+    conf,
+    cursor_null_guard,
+)
 
 class UnaryOperator(BaseEnumeration):
     """
     Describes the UnaryOperator of a declaration
     """
 
-    # The required BaseEnumeration declarations.
-    _kinds = []
-    _name_map = None
-
     def __nonzero__(self):
         """Allows checks of the kind ```if cursor.unary_operator:```"""
         return self.value != 0
 
-    Invalid: "UnaryOperator"
-    PostInc: "UnaryOperator"
-    PostDec: "UnaryOperator"
-    PreInc: "UnaryOperator"
-    PreDec: "UnaryOperator"
-    AddrOf: "UnaryOperator"
-    Deref: "UnaryOperator"
-    Plus: "UnaryOperator"
-    Minus: "UnaryOperator"
-    Not: "UnaryOperator"
-    LNot: "UnaryOperator"
-    Real: "UnaryOperator"
-    Image: "UnaryOperator"
-    Extension: "UnaryOperator"
-    Coawait: "UnaryOperator"
-
-
-UnaryOperator.Invalid = UnaryOperator(0)
-UnaryOperator.PostInc = UnaryOperator(1)
-UnaryOperator.PostDec = UnaryOperator(2)
-UnaryOperator.PreInc = UnaryOperator(3)
-UnaryOperator.PreDec = UnaryOperator(4)
-UnaryOperator.AddrOf = UnaryOperator(5)
-UnaryOperator.Deref = UnaryOperator(6)
-UnaryOperator.Plus = UnaryOperator(7)
-UnaryOperator.Minus = UnaryOperator(8)
-UnaryOperator.Not = UnaryOperator(9)
-UnaryOperator.LNot = UnaryOperator(10)
-UnaryOperator.Real = UnaryOperator(11)
-UnaryOperator.Image = UnaryOperator(12)
-UnaryOperator.Extension = UnaryOperator(13)
-UnaryOperator.Coawait = UnaryOperator(14)
+    Invalid = 0
+    PostInc = 1
+    PostDec = 2
+    PreInc = 3
+    PreDec = 4
+    AddrOf = 5
+    Deref = 6
+    Plus = 7
+    Minus = 8
+    Not = 9
+    LNot = 10
+    Real = 11
+    Image = 12
+    Extension = 13
+    Coawait = 14
 
 
 @property
+@cursor_null_guard
 def unary_operator(self) -> UnaryOperator:
     """
     Retrieves the opcode if this cursor points to a unary operator
@@ -168,4 +45,4 @@ def unary_operator(self) -> UnaryOperator:
 
 Cursor.unary_operator = unary_operator  # type: ignore
 
-__all__ = ["BinaryOperator", "UnaryOperator"]
+__all__ = ["UnaryOperator"]
